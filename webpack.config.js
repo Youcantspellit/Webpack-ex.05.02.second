ea1ccef535d223b976c26cd9f4d86a394e871d8b
Youcantspellit <s.youcantspellit@gmail.com>
2019-02-11 17:56:11 +0100

    ex2

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+
+module.exports = {
+    entry: "./src/js/main.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            }
+        ]
+
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html"
+        }),
+        new CleanWebpackPlugin(['dist'])
+
+        /*      new webpack.ProvidePlugin({
+                 $: 'jquery',
+                 jQuery: 'jquery'
+             }) */
+    ]
+
+}
